659b2e9a8832ed6ad687a326b5ffc1c83682fc7f
Kallil <kallilsouza@hotmail.com>
2022-06-30 06:54:20 -0300

    Actions created in the 'author' view to allow users to follow and unfollow authors

--- mini_twitter/api/views/author.py
+++ mini_twitter/api/views/author.py
@@ -3,21 +3,35 @@ from api.models.author import Author
 from api.serializers.author import AuthorSerializer
 import django_filters
 from django_filters.rest_framework.backends import DjangoFilterBackend
+from rest_framework.decorators import action, authentication_classes
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 
 class AuthorViewSet(viewsets.ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
 
-    http_method_names = ['get', 'post']
+    http_method_names = ['get', 'post']    
 
-    def get_authenticators(self):        
-        if self.request.method == 'POST':
-            authenticators = []
-        else:
-            authenticators = super().get_authenticators()
-        return authenticators
+    @action(methods=['POST'], name='Follow users', detail=True)    
+    def follow(self, request, *args, **kwargs):
+        author = get_object_or_404(Author, pk=kwargs['pk'])  
+        if not request.user.author in author.followers.all():
+            author.followers.add(request.user.author)
+        return HttpResponse(status=200)
 
-    def get_permissions(self):
+    @action(methods=['POST'], name='Unfollow users', detail=True)    
+    def unfollow(self, request, *args, **kwargs):
+        author = get_object_or_404(Author, pk=kwargs['pk'])  
+        if request.user.author in author.followers.all():
+            author.followers.remove(request.user.author)
+        return HttpResponse(status=200)
+
+    @authentication_classes([])
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+
+    def get_permissions(self):        
         if self.request.method == 'POST':
             permissions = []
         else:
